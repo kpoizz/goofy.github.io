931ca1a9990e3864ebbe269c1e5d55c8dd68feea
synapse <109925821+kpoizz@users.noreply.github.com>
2023-02-25 17:27:41 +0000

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+console.log("goofy#2194");
+
+let counter = 0;
+let Title = 'goofy;
+let direction = true;
+aniTitle = setInterval(function() {
+    if (counter == Title.length) direction = false;
+    if (counter == false) direction = true;
+    counter = (direction == true) ? ++counter : --counter;
+    newtitle = (counter == 0) ? "" : Title.slice(0, counter);
+    document.title = 'm' + newtitle;
+}, 400)
+
+window.oncontextmenu = function() {
+    return false;
+}
+$(document).keydown(function(event) {
+    if (event.keyCode == 123) {
+        return false;
+    } else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+        return false;
+    }
+});
+
+function audioPlay() {
+    var audio = document.getElementById("audio");
+    audio.volume = 0.3;
+    audio.play()
+}
+
+function videoPlay() {
+    var video = document.getElementById("video");
+    video.play()
+}
---
+++ assets/js/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed', {
+    strings: ['goofy', 'dev', 'billionare', 'retard', 'roblox', 'idk'],
+    typeSpeed: 40,
+    delaySpeed: 90,
+    showCursor: false,
+    loop: true
+});
